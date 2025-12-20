91d939b6b92ccb177998e99fb334e48d0654099f
Faizahammed Dafedar <faizdafedar3@gmail.com>
2025-12-21 05:29:14 +0530

    updated

---
+++ src/test/java/com/test/utils/RetryAnalyzer.java
@@ -0,0 +1,20 @@
+package com.test.utils;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalyzer implements IRetryAnalyzer {
+
+    private int retryCount = 0;
+    private static final int maxRetryCount = 2; // retry 2 times
+
+    @Override
+    public boolean retry(ITestResult result) {
+
+        if (retryCount < maxRetryCount) {
+            retryCount++;
+            return true;
+        }
+        return false;
+    }
+}
